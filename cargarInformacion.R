58dbefadbecd3b4e920c05c994ea021a8b8122a7
hugoallan9 <hugoallangm@gmail.com>
2014-12-03 22:55:53 -0600

    v 1.1.8
    Version limpia lista para compartir

--- cargarInformacion.R
+++
@@ -1,7 +0,0 @@
-archivo <- file.choose("C:/Users/INE/Documents/")
-separador = ";"
-info <- read.csv2(archivo, sep = separador)
-
-names(info) <- c("Corr", "Titulo", "Descripción", "TGrafica", "desagraga", "Fuente", "Nota")
-
-
